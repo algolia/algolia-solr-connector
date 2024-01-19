f2cc3bacc6e9c411a5d523709851a43624ad4eda
peterdj <peterhale.soundmail@gmail.com>
2024-01-19 12:25:22 -0500

    Remove unused security code

--- src/main/java/com/algolia/connector/bridge/solr/service/impl/AlgoliaServiceImpl.java
+++ src/main/java/com/algolia/connector/bridge/solr/service/impl/AlgoliaServiceImpl.java
@@ -29,7 +29,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
-import java.security.AccessController;
 
 /**
  * @author Rakesh Kumar
